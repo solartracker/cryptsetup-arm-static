4d4baa4dc7b02ef613269e319dc6e85878a443e3
Richard Elwell <229418400@users.noreply.github.com>
2026-03-01 11:15:54 -0500

    fix portable directory

--- files/man-db/man-db-2.13.1/solartracker/install.sh
+++ files/man-db/man-db-2.13.1/solartracker/install.sh
@@ -4,24 +4,38 @@
 #
 # Setup the runtime environment for portable man-db
 #
-# Must be sourced:
-#   . install.sh
-#        -OR-
-#   source install.sh
-#
 # Copyright (C) 2025 Richard Elwell
 # Licensed under GPLv3 or later
 ################################################################################
-(return 0 2>/dev/null) || {
-    echo "ERROR: This script must be sourced:"
-    echo "  . install.sh"
-    echo "  source install.sh"
-    exit 1
-}
-
+PATH_CMD="$(readlink -f -- "$0")"
 SCRIPT_DIR="$(cd -- "$(dirname -- "$0")" && pwd -P)"
 PKG_ROOT="mandb"
 PORTABLE_DIR="/tmp/portable-${PKG_ROOT}"
+
+main() {
+    if [ -n "${PATH_CMD}" ]; then
+        echo "ERROR: This script must be sourced:"
+        echo "source install.sh"
+        echo "    -OR-"
+        echo ". install.sh"
+        echo ""
+        return 1
+    fi
+
+    if [ ! -d "${PORTABLE_DIR}" ]; then
+        ln -snf "${SCRIPT_DIR}" "${PORTABLE_DIR}"
+    fi
+
+    if ! contains_path "${PORTABLE_DIR}/libexec"; then
+        export PATH="${PORTABLE_DIR}/libexec:${PATH}"
+    fi
+
+    if ! contains_path "${PORTABLE_DIR}/bin"; then
+        export PATH="${PORTABLE_DIR}/bin:${PATH}"
+    fi
+
+    return 0
+}
 
 contains_path() {
     case ":${PATH}:" in
@@ -30,17 +44,5 @@ contains_path() {
     esac
 }
 
-if [ ! -d "${PORTABLE_DIR}" ]; then
-    ln -snf "${SCRIPT_DIR}" "${PORTABLE_DIR}"
-fi
-
-if ! contains_path "${PORTABLE_DIR}/libexec"; then
-    PATH="${PORTABLE_DIR}/libexec:${PATH}"
-fi
-
-if ! contains_path "${PORTABLE_DIR}/bin"; then
-    PATH="${PORTABLE_DIR}/bin:${PATH}"
-fi
-
-export PATH
+main
 
