ad01f1e2abfb88133842c266203ddfb449aa574f
Richard Elwell <229418400@users.noreply.github.com>
2026-03-01 10:39:18 -0500

    fix portable directory

--- files/man-db/man-db-2.13.1/solartracker/install.sh
+++ files/man-db/man-db-2.13.1/solartracker/install.sh
@@ -4,37 +4,43 @@
 #
 # Setup the runtime environment for portable man-db
 #
+# Must be sourced:
+#   . install.sh
+#        -OR-
+#   source install.sh
+#
 # Copyright (C) 2025 Richard Elwell
 # Licensed under GPLv3 or later
 ################################################################################
-SCRIPT_DIR="$(dirname -- "$(readlink -f -- "$0")")"
-PKG_ROOT="mandb"
-PORTABLE_DIR="/tmp/portable-${PKG_ROOT}"
-set -e
-set -x
-
-main() {
-    if [ ! -d "${PORTABLE_DIR}" ]; then
-        ln -snf "${SCRIPT_DIR}" "${PORTABLE_DIR}"
-    fi
-
-    if ! contains "${PATH}" "${PORTABLE_DIR}/libexec"; then
-        export PATH="${PORTABLE_DIR}/libexec:${PATH}"
-    fi
-
-    if ! contains "${PATH}" "${PORTABLE_DIR}/bin"; then
-        export PATH="${PORTABLE_DIR}/bin:${PATH}"
-    fi
-
-    return 0
+(return 0 2>/dev/null) || {
+    echo "ERROR: This script must be sourced:"
+    echo "  . install.sh"
+    echo "  source install.sh"
+    exit 1
 }
 
-contains() {
-    case "$1" in
-        *"$2"*) return 0 ;;
-        *)      return 1 ;;
+SCRIPT_DIR="$(cd -- "$(dirname -- "$0")" && pwd -P)"
+PKG_ROOT="mandb"
+PORTABLE_DIR="/tmp/portable-${PKG_ROOT}"
+
+contains_path() {
+    case ":${PATH}:" in
+        *":${1}:"*) return 0 ;;
+        *)          return 1 ;;
     esac
 }
 
-main
+if [ ! -d "${PORTABLE_DIR}" ]; then
+    ln -snf "${SCRIPT_DIR}" "${PORTABLE_DIR}"
+fi
+
+if ! contains_path "${PORTABLE_DIR}/libexec"; then
+    PATH="${PORTABLE_DIR}/libexec:${PATH}"
+fi
+
+if ! contains_path "${PORTABLE_DIR}/bin"; then
+    PATH="${PORTABLE_DIR}/bin:${PATH}"
+fi
+
+export PATH
 
